21ecf4c6b73adb72831a685591a95daf8d19c234
chhavid <99489321+chhavid@users.noreply.github.com>
2022-06-29 10:30:11 +0530

    Moved data in src

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const { handleRequest, addGuestBook } = require('./AppHandlers/handler.js');
 const { serveFileContent, notFound } = require('./AppHandlers/serveFileHandler.js');
 
-const commentsFile = './public/data/comments.json';
-const guestBookTemplate = './public/data/guestbook.html'
+const commentsFile = './src/data/comments.json';
+const guestBookTemplate = './src/data/guestbook.html'
 const handlers = [addGuestBook(commentsFile, guestBookTemplate), handleRequest, serveFileContent, notFound];
 
 module.exports = { handlers };
